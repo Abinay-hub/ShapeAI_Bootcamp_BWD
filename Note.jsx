f83955cb296255117dd015b76a8fd2c70cf00061
Abinay-hub <85342256+Abinay-hub@users.noreply.github.com>
2021-06-06 22:08:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Web development</h1>
+      <p>
+        In this bootcamp,we learn all the basic programming languages required
+        for developing a web page like HTML,CSS and JavaScript.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
